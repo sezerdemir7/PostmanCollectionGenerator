5bb1c2b08e0a1f1fedca2d73341136f2e229380a
sezerdemir7 <ddemir1221@gmail.com>
2024-12-04 19:38:11 +0300

    update code

--- src/main/java/com/demir/postmancollectiongenerator/controller/DemoController.java
+++
@@ -1,9 +0,0 @@
-package com.demir.postmancollectiongenerator.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("v1/demo")
-public class DemoController {
-}
